5e203dff68bf38f299d5b4466d44fd150f1595ee
andres085 <andresito.martinez1985@gmail.com>
2022-03-03 22:49:15 -0300

    Se instalo vue y se probo con un componente de ejemplo

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+
+import App from './components/App.vue';
+
+const app = createApp({
+
+});
+
+app.component('example-component', App);
+
+app.mount('#app');
